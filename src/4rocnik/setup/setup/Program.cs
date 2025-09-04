c358dc0f962534b345cdb8f96da07168135992fd
Matěj Šilhán <matej.silhan@student.spsmb.cz>
2025-09-04 11:02:26 +0200

    fizzbuzz

--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -1,9 +1,35 @@
+using System;
+
 namespace setup
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                FizzBuzz(i);
+            }
+        }
+
+        public static void FizzBuzz(int num)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
         }
     }
 }
