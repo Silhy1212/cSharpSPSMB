4d9bdd31be4b32cd726ce96a3a8b2bd50ab36096
Matěj Šilhán <matej.silhan@student.spsmb.cz>
2025-09-04 15:11:11 +0200

    blud kalkulacka bez testu

---
+++ src/4rocnik/setup/TestProject1/Tests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
--- src/4rocnik/setup/setup/Program.cs
+++ src/4rocnik/setup/setup/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace setup
 {
@@ -6,7 +8,7 @@ namespace setup
     {
         public static void Main(string[] args)
         {
-            GuessNum guessNum = new GuessNum(1, 100);
+           /* GuessNum guessNum = new GuessNum(1, 100);
             int randomNum = guessNum.RandNum();
             string RandomNum = randomNum.ToString();
             while (!guessNum.guessedNum)
@@ -23,9 +25,70 @@ namespace setup
                     Console.WriteLine("You lose!");
                     Console.WriteLine("You guessed the wrong number!");
                 }
-            }
+            }*/
+
+           Console.WriteLine(InputParse(Console.ReadLine()));
+           
         }
 
+
+        public static double Calculator(double num, double num2, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return num + num2;
+                case "-":
+                    return num - num2;
+                case "/":
+                    return num / num2;
+                case "*":
+                    return num * num2;
+                case "**":
+                    return Math.Pow(num, num2);
+                default:
+                    return 0;
+            } 
+        }
+
+        public static double InputParse(string input)
+        {
+            List<string> blud = input.Split(' ').ToList();
+            blud.RemoveAll(x => x == " ");
+            return Calculator(double.Parse(blud[0]), double.Parse(blud[2]), blud[1]);
+            
+        }
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         public static void FizzBuzz(int num)
         {
             if (num % 3 == 0 && num % 5 == 0)
